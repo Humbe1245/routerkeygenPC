81e5b1bd769aeebffaa41606535bebf4d6a86c5a
Humbe <22565720+Humbe1245@users.noreply.github.com>
2022-09-03 02:47:47 -0500

    deleting duplicate code

--- src/algorithms/InterCableKeygen.cpp
+++ src/algorithms/InterCableKeygen.cpp
@@ -21,8 +21,6 @@ QVector<QString> & InterCableKeygen::getKeys() {
     int last = mac.right(2).toInt(0, 16);
     mac = shortMac + QString("%1").arg(last + 1, 2, 16, QLatin1Char('0')).right(2);
     results.append("m" + mac.toLower());
-    mac = shortMac + QString("%1").arg(last + 2, 2, 16, QLatin1Char('0')).right(2);
-    results.append("m" + mac.toLower());
 
 	return results;
 }
